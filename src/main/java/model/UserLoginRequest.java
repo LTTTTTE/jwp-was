c0c5951f609cd3c5a2f9dcd277e0e604a667fa8c
juyeongjung <juyeong.dev@gmail.com>
2020-11-19 15:03:41 +0900

    feat: User create, login Request dto 분리

---
+++ src/main/java/model/UserLoginRequest.java
@@ -0,0 +1,23 @@
+package model;
+
+public class UserLoginRequest {
+
+    private String userId;
+    private String password;
+
+    public UserLoginRequest() {
+    }
+
+    public UserLoginRequest(String userId, String password) {
+        this.userId = userId;
+        this.password = password;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
